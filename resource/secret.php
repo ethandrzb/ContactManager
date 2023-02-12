3fbb42049676597bea37e558527f11ed9612b09f
Ethan Thomas <ethandrzb@gmail.com>
2023-02-12 17:40:59 -0500

    Change signing key

--- resource/secret.php
+++ resource/secret.php
@@ -4,7 +4,7 @@
     use Lcobucci\JWT\Signer\Key\InMemory;
 
     $key = InMemory::base64Encoded(
-        'U29tZXJzZXQtUHJvcG9zZS1FbXBsb3llZS1UcmFuc2Zvcm1hdGlvbi1BcHBlYWxzLUhlc2l0YXRlZC1FYXN0ZXItVmlydHVlLTE='
+        'no-key-for-you'
     );
 
 ?>
