8d3f565da7091bc66668770d99fe89c0661d2e7c
Ethan Thomas <ethandrzb@gmail.com>
2023-02-13 21:35:24 -0500

    Convert validate_jwt to function

--- resource/validate_jwt.php
+++ resource/validate_jwt.php
@@ -15,31 +15,31 @@ use Lcobucci\JWT\Encoding\CannotDecodeContent;
 use Lcobucci\JWT\Validation\Validator;
 
 require 'vendor/autoload.php';
-require('secret.php');
 
-$parser = new Parser(new JoseEncoder());
-
-try
+function validateJWT($tokenString)
 {
-    $token = $parser->parse(
-    // Valid token
-    // "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzYyNDA2NDEsIm5iZiI6MTY3NjI0MDY0MSwiZXhwIjoxNjc2MjgzODQxLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L0NvbnRhY3RNYW5hZ2VyL3Jlc291cmNlL2xvZ2luLnBocCIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3QvQ29udGFjdE1hbmFnZXIvcmVzb3VyY2UiLCJ1c2VySUQiOjl9.LSK8PuOHQUJxDjwt89j0fb6CCNCSxdQzNSqRf6mqGCA"
+    require('secret.php');
+    $parser = new Parser(new JoseEncoder());
 
-    // Valid, but expired token
-    // "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.esyJpYXQiOjE2NzYyNDA2NDEsIm5iZiI6MTY3NjI0MDY0MSwiZXhwIjoxNjc2MjQwMDQxLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L0NvbnRhY3RNYW5hZ2VyL3Jlc291cmNlL2xvZ2luLnBocCIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3QvQ29udGFjdE1hbmFnZXIvcmVzb3VyY2UiLCJ1c2VySUQiOjl9.p2V0lcGJ9SRDdw5mJFYZrvuKKl3ocPjv0g9cW8PnZ-s"
+    try
+    {
+        $token = $parser->parse($tokenString);
+    
+        $validator = new Validator();
+        $validator->assert($token, new StrictValidAt(new SystemClock(new DateTimeZone('UTC'))));
+        $validator->assert($token, new SignedWith(new Sha256(), $key));
+    
+        return True;
+    } catch(CannotDecodeContent $e)
+    {
+        echo "Invalid token";
+    } catch (RequiredConstraintsViolated $e) {
+        // list of constraints violation exceptions:
+        var_dump($e->violations());
+    }
 
-    // Bad token
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzYyNDA2EsIm5iZiI6MTY3NjI0MDY0MSwiZXhwIjoxNjc2MjgzODQxLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L0NvbnRhY3RNYW5hZ2VyL3Jlc291cmNlL2xvZ2luLnBocCIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3QvQ29udGFjdE1hbmFnZXIvcmVzb3VyY2UiLCJ1c2VySUQiOjl9.LSK8PuOHQUJxDjwt89j0fb6CCNCSxdQzNSqRf6mqGCA"
-    );
-
-    $validator = new Validator();
-
-    $validator->assert($token, new StrictValidAt(new SystemClock(new DateTimeZone('UTC'))));
-    $validator->assert($token, new SignedWith(new Sha256(), $key));
-} catch(CannotDecodeContent $e)
-{
-    echo "Invalid token";
-} catch (RequiredConstraintsViolated $e) {
-    // list of constraints violation exceptions:
-    var_dump($e->violations());
+    return False;
 }
+
+// var_dump(validateJWT("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzYzNDAxMTAsIm5iZiI6MTY3NjM0MDExMCwiZXhwIjoxNjc2MzgzMzEwLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L0NvbnRhY3RNYW5hZ2VyL3Jlc291cmNlL2xvZ2luLnBocCIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3QvQ29udGFjdE1hbmFnZXIvcmVzb3VyY2UiLCJ1c2VySUQiOjEwfQ.Wp-AUEJU8fKnWo4s6O23ei1dqSv7fWo92ar4Zp_22yY"));
+?>
