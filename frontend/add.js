8dac7e0c7c4d4f82e8b12a9b5d0c5ed2ec017413
Nikole Solano <88204279+nikoles123@users.noreply.github.com>
2023-03-09 17:49:24 -0500

    Add files via upload

---
+++ frontend/add.js
@@ -0,0 +1,35 @@
+var date = new Date();
+const todayDate = document.querySelector('.date');
+
+function printDate(){
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth()+1;
+    var year = date.getFullYear();
+    todayDate.innerHTML = "Added on: " + month + "/" + day + "/" + year;
+}
+printDate();
+
+const addForm = document.getElementById("add");
+const addButton = document.getElementById("add-submit");
+
+addForm.addEventListener("submit", (event) => {
+  const firstname = addForm.firstname.value;
+  const lastname = addForm.lastname.value;
+  const email = addForm.email.value;
+  const phone = addForm.phone.value;
+
+  var raw = "{\r\n    \"firstName\" : \" " + firstname + "\",\r\n    \"lastName\" : \"\" "+ lastname + ",\r\n    \"email\" : \"\" "+ email + ",\r\n    \"phone\" : \"\" "+ phone +"\r\n}";
+
+    var requestOptions = {
+    method: 'POST',
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("http://cop4331-2023.xyz/resource/addContact.php", requestOptions)
+    .then(response => {
+        window.location.href = window.location.href + "/contacts.html"
+    })
+    event.preventDefault();
+})
