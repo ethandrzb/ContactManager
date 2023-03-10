b519a9bc4400f59c010b96b24cc3c17e7889a5c2
Ruth Pearl <ruth.j.pearl@gmail.com>
2023-03-10 15:30:19 -0500

    Working (?) delete button

--- frontend/details.js
+++ frontend/details.js
@@ -6,3 +6,18 @@ detailsForm.addEventListener("submit", (event) => {
     window.location.href = "/edit.html?cid=" + id;
     event.preventDefault();
 })
+
+function deleteUser() {
+    var raw = "{\r\n    \"contactID\" : " + id + "\r\n}";
+
+    var requestOptions = {
+        method: 'POST',
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("http://cop4331-2023.xyz/resource/deleteContact.php", requestOptions)
+    .then(response => response.text())
+    .then(result => window.location.href = "/contacts.html")
+    .catch(error => console.log('error', error));    
+}
