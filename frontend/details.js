749e5bcf921e726ac09517b6c5cf0b79c4a6715b
Nikole Solano <88204279+nikoles123@users.noreply.github.com>
2023-03-11 20:13:59 -0500

    Add files via upload

--- frontend/details.js
+++ frontend/details.js
@@ -2,30 +2,30 @@ const id = new URLSearchParams(window.location.search).get('cid');
 
 const detailsForm = document.getElementById("detailsForm");
 
-detailsForm.addEventListener("submit", (event) => { 
+var raw = "{\r\n    \"contactID\" : \"" + id + "\"\r\n}";
 
-    var raw = "{\r\n    \"contactID\" : \"" + id + "\"\r\n}";
+var requestOptions = {
+    method: 'POST',
+    body: raw,
+    redirect: 'follow'
+    };
 
-    var requestOptions = {
-        method: 'POST',
-        body: raw,
-        redirect: 'follow'
-      };
-
-    window.location.href = "/edit.html?cid=" + id;
-    event.preventDefault();
-
-    fetch("https://cop4331-2023.xyz/resource/getContact.php", requestOptions)
+fetch("https://cop4331-2023.xyz/resource/getContact.php", requestOptions)
     .then(response => response.text())
     .then(response => {
         //response may be diff too
         let contact = JSON.parse(response);
-        document.getElementById('first').innerHTML = contact.firstname;
-        document.getElementById('last').innerHTML = contact.lastname;
-        document.getElementById('email').innerHTML = contact.email;
-        document.getElementById('phone').innerHTML = contact.phone;
-        document.getElementById('date').innerHTML = contact.dateCreated;
+        document.getElementById('first').innerHTML = contact[0][0];
+        document.getElementById('last').innerHTML = contact[0][1];
+        document.getElementById('email').innerHTML = contact[0][2];
+        document.getElementById('phone').innerHTML = contact[0][3];
+        document.getElementById('date').innerHTML = "Added on: " + contact[0][4];
     })
+
+detailsForm.addEventListener("submit", (event) => { 
+
+    window.location.href = "/edit.html?cid=" + id;
+    event.preventDefault();
 })
 
 function deleteUser() {
