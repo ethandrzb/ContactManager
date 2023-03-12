6fa75fda271ff6904066fe0568ce3723f5a6c714
Nikole Solano <88204279+nikoles123@users.noreply.github.com>
2023-03-11 20:26:07 -0500

    Add files via upload

--- frontend/edit.js
+++ frontend/edit.js
@@ -1,5 +1,26 @@
 const editForm = document.getElementById("edit");
 const editButton = document.getElementById("edit-submit");
+const id = new URLSearchParams(window.location.search).get('cid');
+
+var raw = "{\r\n    \"contactID\" : \"" + id + "\"\r\n}";
+    
+var requestOptions = {
+    method: 'POST',
+    body: raw,
+    redirect: 'follow'
+};
+
+fetch("https://cop4331-2023.xyz/resource/getContact.php", requestOptions)
+.then(response => response.text())
+.then(response => {
+    //response may be diff too
+    let contact = JSON.parse(response);
+    editForm.firstname.value = contact[0][0];
+    editForm.lastname.value = contact[0][1];
+    editForm.email.value = contact.email[0][2];
+    editForm.phone.value = contact.phone[0][3];
+    editForm.date.value = "Date Added: " + contact.dateCreated[0][4];
+})
 
 //this will actually update the contact
 editForm.addEventListener("submit", (event) => {
@@ -11,23 +32,6 @@ editForm.addEventListener("submit", (event) => {
     const id = new URLSearchParams(window.location.search).get('cid');
   
     var raw = "{\r\n    \"contactID\" :\"" + id + "\",\r\n    \"firstName\" : \"" + firstname + "\",\r\n    \"lastName\" : \"" + lastname + "\",\r\n    \"email\" : \"" + email + "\",\r\n    \"phone\" : \"" + phone + "\"\r\n}";
-    
-    var requestOptions = {
-        method: 'POST',
-        body: raw,
-        redirect: 'follow'
-    };
-    
-    fetch("https://cop4331-2023.xyz/resource/getContact.php", requestOptions)
-    .then(response => response.text())
-    .then(response => {
-        //response may be diff too
-        let contact = JSON.parse(response);
-        editForm.firstname.value = contact.firstname;
-        editForm.lastname.value = contact.lastname;
-        editForm.email.value = contact.email;
-        editForm.phone.value = contact.phone;
-    })
     
 
     fetch("https://cop4331-2023.xyz/resource/editContact.php", requestOptions)
