28d00ce79e3a5bdfcf754d7aeb07ba29fe90f8ff
Brooks McKinley <31021010+brooksmckinley@users.noreply.github.com>
2023-03-19 19:49:30 -0400

    Final merge frontend into master (#8)
    * Add files via upload
    
    * Update details.html
    
    * Update details.html
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update contacts.html
    
    * Update contacts.html
    
    * Update contacts.html
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update details.js
    
    ---------
    
    Co-authored-by: Nikole Solano <88204279+nikoles123@users.noreply.github.com>

--- edit.js
+++ edit.js
@@ -17,8 +17,8 @@ fetch("https://cop4331-2023.xyz/resource/getContact.php", requestOptions)
     let contact = JSON.parse(response);
     editForm.firstname.value = contact[0][0];
     editForm.lastname.value = contact[0][1];
-    editForm.email.value = contact[0][3];
-    editForm.phone.value = contact[0][2];
+    editForm.email.value = contact[0][2];
+    editForm.phone.value = contact[0][3];
     editForm.date.value = "Date Added: " + contact[0][4];
 })
 
--- frontend/details.js
+++ frontend/details.js
@@ -2,20 +2,30 @@ const id = new URLSearchParams(window.location.search).get('cid');
 
 const detailsForm = document.getElementById("detailsForm");
 
-detailsForm.addEventListener("submit", (event) => { 
-    window.location.href = "/edit.html?cid=" + id;
-    event.preventDefault();
+var raw = "{\r\n    \"contactID\" : \"" + id + "\"\r\n}";
 
-    fetch("https://cop4331-2023.xyz/resource/getContact.php", requestOptions)
+var requestOptions = {
+    method: 'POST',
+    body: raw,
+    redirect: 'follow'
+    };
+
+fetch("https://cop4331-2023.xyz/resource/getContact.php", requestOptions)
     .then(response => response.text())
     .then(response => {
         //response may be diff too
         let contact = JSON.parse(response);
-        document.getElementById('first').innerHTML = contact.firstname;
-        document.getElementById('last').innerHTML = contact.lastname;
-        document.getElementById('email').innerHTML = contact.email;
-        document.getElementById('phone').innerHTML = contact.phone;
+        document.getElementById('first').innerHTML = "<b>First: </b>" + contact[0][0];
+        document.getElementById('last').innerHTML = "<b>Last: </b>" + contact[0][1];
+        document.getElementById('email').innerHTML = "<b>Email: </b>" + contact[0][3];
+        document.getElementById('phone').innerHTML = "<b>Phone: </b>" + contact[0][2];
+        document.getElementById('date').innerHTML = "<b>Added on: </b>" + contact[0][4];
     })
+
+detailsForm.addEventListener("submit", (event) => { 
+
+    window.location.href = "/edit.html?cid=" + id;
+    event.preventDefault();
 })
 
 function deleteUser() {
